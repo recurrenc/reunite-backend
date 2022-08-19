f70b245460bc5f0d2c1107316f06dd74e9f6d257
Sonu Kumar <sonu.patna0808@gmail.com>
2022-08-19 14:43:02 +0530

    start script

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 const app = express();
+const port = process.env.PORT || 4000;
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(4000, () => console.log("Server is running on port 4000 "));
+app.listen(port, () => console.log("Server is running on port 4000 "));
